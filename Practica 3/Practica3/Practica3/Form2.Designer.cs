a71f34d013d9863d2c347cbc90601a7d3ffe98e6
Pablo Girón <127716000+Kratos07p@users.noreply.github.com>
2023-09-04 22:16:54 -0600

    Add files via upload

---
+++ Practica 3/Practica3/Practica3/Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Practica3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnIniciar = new Button();
+            SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Font = new Font("Arial Black", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnIniciar.Location = new Point(93, 67);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(328, 298);
+            btnIniciar.TabIndex = 0;
+            btnIniciar.Text = "Iniciar";
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(528, 450);
+            Controls.Add(btnIniciar);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnIniciar;
+    }
+}
